6586991054f075192510b3fd99bdfb171ba20f3a
Nazmus <nazmus.nasir@gmail.com>
2016-05-21 19:56:42 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+config.php 
+Title: ANUSTART
+Author: Nazmus
+URL: http://nazm.us
+Github: https://github.com/nlinux1/
+
+MySQL Database Configuration File
+*/
+
+$server = "localhost"; //server name/url
+$username = "nustart"; //MySQL Username
+$password = ""; //MySQL Password 
+$db = "nustart"; // MySQL Database name
+
+// Create connection
+$conn = mysqli_connect($server, $username, $password, $db);
+
+if(mysqli_connect_errno()){
+		echo "DB Connection failed: ". mysqli_connect_error(); //error checking
+	} 
+
+?>
